488d5d8fa95baea0f639eb396d70ef3b225a49b4
Bemly <evan93.mitchell@gmail.com>
2022-01-17 16:57:45 -0500

    Add User model.

--- speak/models.py
+++ speak/models.py
@@ -1,9 +1,13 @@
 from django.db import models
+from django.conf import settings
 import random
+
+User = settings.AUTH_USER_MODEL
 
 class Speak(models.Model):
     # id = models.AutoField(primary_key=True)   is behind the scenes
      # blank=True means not required in Django, null=True not required in database
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
