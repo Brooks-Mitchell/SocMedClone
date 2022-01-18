5af7749f01773c8b959c84b0890412d64c199c6e
Bemly <evan93.mitchell@gmail.com>
2022-01-18 16:14:16 -0500

    Add likes to database and to admin view

--- speak/admin.py
+++ speak/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from .models import Speak
+from .models import Speak, SpeakLike
+
+
+class SpeakLikeAdmin(admin.TabularInline):
+     model = SpeakLike
 
 # adds a search box the the admin, and displays the username for associated posts
 class SpeakAdmin(admin.ModelAdmin):
+    inlines = [SpeakLikeAdmin]
     list_display =['__str__', 'user']
     search_fields = ['content', 'user__username', 'user__email']
     class Meta:
--- speak/models.py
+++ speak/models.py
@@ -4,12 +4,19 @@ import random
 
 User = settings.AUTH_USER_MODEL
 
+class SpeakLike(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    speak = models.ForeignKey("Speak", on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+
 class Speak(models.Model):
     # id = models.AutoField(primary_key=True)   is behind the scenes
      # blank=True means not required in Django, null=True not required in database
     user = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(User, related_name='speak_user', blank=True, through=SpeakLike)
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
 
 
 
